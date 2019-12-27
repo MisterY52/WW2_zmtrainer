857ac2658506b54b9c6659c4e667d73c99f79bdf
Andrea <andre_9612@yahoo.it>
2019-12-27 10:24:56 +0100

    First commit

---
+++ WW2_zmtrainer/address.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zm
+{
+    class address
+    {
+        public static Int64 baseadd = 0;
+        public static Int64 soldi;
+        public static Int64[] ammo = new Int64[14];
+        public static Int64[] ammo_init = new Int64[14];
+        public static Int64 vita;
+        public static Int64 special;
+        public static Int64[] vz = new Int64[21];
+        public static Int64[] posz_X = new Int64[21];
+        public static Int64[] posz_Y = new Int64[21];
+        public static Int64[] posz_Z = new Int64[21];
+        public static Int64 give1;
+        public static Int64 give2;
+        public static Int64 give3;
+        public static Int64 rapid;
+        public static Int64 jump;
+        public static Int64 X;
+        public static Int64 Y;
+        public static Int64 Z;
+        public static Int64 norecoil;
+        public static Int64 curweap;
+        public static Int64 timescale;
+        public static Int64 round;
+        public static Int64 nome;
+
+        public class player
+        {
+            public Int64 soldi;
+            public Int64[] ammo = new Int64[14];
+            public Int64[] ammo_init = new Int64[14];
+            public Int64 vita;
+            public Int64 special;
+            public Int64 give1;
+            public Int64 give2;
+            public Int64 give3;
+            public Int64 rapid;
+            public Int64 X;
+            public Int64 Y;
+            public Int64 Z;
+            public Int64 nome;
+        }
+
+        public static player p2 = new player();
+        public static player p3 = new player();
+        public static player p4 = new player();
+
+        public static void findadd(string s)
+        {
+            memory m = new memory();
+            m.AttackProcess("s2_mp64_ship");
+            
+            try
+            {
+                switch (s)
+                {
+                    case "money_p1":
+                        soldi = m.FindPattern(baseadd + 0xA000000, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "????xxx????xxxxxx?");
+                        soldi = m.FindPattern(soldi+1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "????xxx????xxxxxx?");
+                        break;
+                    case "money_p2":
+                        p2.soldi = m.FindPattern(baseadd + 0xA000000, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        break;
+                    case "money_p3":
+                        p3.soldi = m.FindPattern(baseadd + 0xA000000, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        p3.soldi = m.FindPattern(p3.soldi+1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        p3.soldi = m.FindPattern(p3.soldi + 1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        break;
+                    case "money_p4":
+                        p4.soldi = m.FindPattern(baseadd + 0xA000000, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        p4.soldi = m.FindPattern(p4.soldi + 1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        p4.soldi = m.FindPattern(p4.soldi + 1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        p4.soldi = m.FindPattern(p4.soldi + 1, baseadd + 0xF000000, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x6B\x00\x00\x31\x80", "???????????xxxxxx?");
+                        break;
+                    case "round":
+                        round = m.FindPattern(baseadd + 0xA000000, baseadd + 0xF000000, "\x03\x00\x00\x00\x00\x00\x00\x00\xE6\x29\x00\x00\x06\xC0\x00\x00\x54\x80", "????xxxx????xxxxx?");
+                        break;
+                    default: break;
+                }
+            }
+            catch
+            {
+            }
+        }
+        //+119600, vita
+        //+11A600, ammo, rapid, coord, give
+        //+29ACF0
+        //+29AFF0  3C9A80
+        public static void setadd()
+        {
+            memory m = new memory();
+            m.AttackProcess("s2_mp64_ship");
+            baseadd = memory.GetBaseAddress("s2_mp64_ship").ToInt64();
+            for (int i = 0; i < ammo.Length; i++)
+            {
+                ammo[i] = baseadd + 0xA40D688 + i * 0x18; 
+                ammo_init[i] = ammo[i] + 0x8;
+            }
+            vita = baseadd + 0xA024B0C; 
+            special = baseadd + 0xA40EF88;
+            give1 = baseadd + 0xA40D35C;
+            give2 = give1 + 0x24; //baseadd + 0xA10E440;
+            give3 = give2 + 0xC; //baseadd + 0x9DBBECC;
+            rapid = baseadd + 0xA40D30C;
+            jump = baseadd + 0xC33F48;
+            X = baseadd + 0xA40CFA4;
+            Y = X + 0x4;
+            Z = X + 0x8;
+            norecoil = baseadd + 0x51B71;
+            curweap = baseadd + 0x5CC7480;
+            timescale = baseadd + 0x278FB7C; //00 00 80 3F 01 00 00 00 00 00 00 00
+            for (int i = 0; i < vz.Length; i++)
+            {
+                vz[i] = baseadd + 0xA030F8C + 0x418 * i; //150 default, round 1
+            }
+            for (int i = 0; i < posz_X.Length; i++)
+            {
+                posz_X[i] = baseadd + 0xA534324 + 0x5CC8 * i;
+                posz_Y[i] = posz_X[i] + 0x4;
+                posz_Z[i] = posz_X[i] + 0x8;
+            }
+            nome = baseadd + 0x9DC149A;
+            //Player 2
+            //+6244, ammo, coord, give, rapid, special
+            //+6640, soldi
+            //+418, vita
+            p2.vita = vita + 0x418;
+            for (int i = 0; i < ammo.Length; i++)
+            {
+                p2.ammo[i] = ammo[i] + 0x6268;
+                p2.ammo_init[i] = ammo_init[i] + 0x6268;
+            }
+            p2.X = X + 0x6268;
+            p2.Y = p2.X + 0x4;
+            p2.Z = p2.X + 0x8;
+            p2.give1 = give1 + 0x6268;
+            p2.give2 = give2 + 0x6268;
+            p2.give3 = give3 + 0x6268;
+            p2.rapid = rapid + 0x6268;
+            p2.special = special + 0x6268;
+            p2.nome = nome + 0x6268;
+            //Player 3
+            p3.vita = vita + 0x418 * 2;
+            for (int i = 0; i < ammo.Length; i++)
+            {
+                p3.ammo[i] = ammo[i] + 0x6268 * 2;
+                p3.ammo_init[i] = ammo_init[i] + 0x6268 * 2;
+            }
+            p3.X = X + 0x6268 * 2;
+            p3.Y = p3.X + 0x4;
+            p3.Z = p3.X + 0x8;
+            p3.give1 = give1 + 0x6268 * 2;
+            p3.give2 = give2 + 0x6268 * 2;
+            p3.give3 = give3 + 0x6268 * 2;
+            p3.rapid = rapid + 0x6268 * 2;
+            p3.special = special + 0x6268 * 2;
+            p3.nome = nome + 0x6268 * 2;
+            //Player 4
+            p4.vita = vita + 0x418 * 3;
+            for (int i = 0; i < ammo.Length; i++)
+            {
+                p4.ammo[i] = ammo[i] + 0x6268 * 3;
+                p4.ammo_init[i] = ammo_init[i] + 0x6268 * 3;
+            }
+            p4.X = X + 0x6268 * 3;
+            p4.Y = p4.X + 0x4;
+            p4.Z = p4.X + 0x8;
+            p4.give1 = give1 + 0x6268 * 3;
+            p4.give2 = give2 + 0x6268 * 3;
+            p4.give3 = give3 + 0x6268 * 3;
+            p4.rapid = rapid + 0x6268 * 3;
+            p4.special = special + 0x6268 * 3;
+            p4.nome = nome + 0x6268 * 3;
+        }
+    }
+}
---
+++ WW2_zmtrainer/memory.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+
+namespace zm
+{
+    public class memory
+    {
+        public const uint PROCESS_VM_READ = 0x0010;
+        public const uint PROCESS_VM_WRITE = 0x0020;
+        public const uint PROCESS_VM_OPERATION = 0x0008;
+        public const uint PAGE_READWRITE = 0x0004;
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(UInt32 dwAccess, bool inherit, int pid);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, Int64 lpBaseAddress, [In, Out] byte[] lpBuffer, UInt64 dwSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, Int64 lpBaseAddress, [In, Out] byte[] lpBuffer, UInt64 dwSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, UInt32 dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UInt32 dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+            private Process CurProcess;
+            private IntPtr ProcessHandle;
+            private string ProcessName;
+            private int ProcessID;
+            public IntPtr BaseModule;
+
+            // Destruktor
+            ~memory() { if (ProcessHandle != IntPtr.Zero) memory.CloseHandle(ProcessHandle); }
+
+            // Get Process for work
+            public bool AttackProcess(string _ProcessName)
+            {
+            try
+            {
+                Process[] Processes = Process.GetProcessesByName(_ProcessName);
+
+                if (Processes.Length > 0)
+                {
+                    BaseModule = Processes[0].MainModule.BaseAddress;
+                    CurProcess = Processes[0];
+                    ProcessID = Processes[0].Id;
+                    ProcessName = _ProcessName;
+
+                    ProcessHandle = memory.OpenProcess(memory.PROCESS_VM_READ | memory.PROCESS_VM_WRITE | memory.PROCESS_VM_OPERATION, false, ProcessID);
+                    if (ProcessHandle != IntPtr.Zero)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            }
+
+            // If Process attacked
+            public bool IsOpen()
+            {
+                if (ProcessName == string.Empty)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (AttackProcess(ProcessName))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            internal static IntPtr GetBaseAddress(string ProcessName)
+            {
+                try
+                {
+                    Process[] L2Process = Process.GetProcessesByName(ProcessName);
+                    return L2Process[0].MainModule.BaseAddress;
+                }
+                catch { return IntPtr.Zero; }
+            }
+        
+            public Int64 FindPattern(Int64 startAddress, Int64 endAddress, string pattern, string mask)
+            {
+                IntPtr bytes;
+                var buffer = new byte[endAddress - startAddress];
+                memory.ReadProcessMemory(ProcessHandle, startAddress, buffer, (ulong)buffer.Length, out bytes);
+
+                for (Int64 i = 0; i < buffer.Length; i++)
+                {
+                    for (int x = 0; x < pattern.Length; x++)
+                    {
+                        if (buffer[i + x] == pattern[x] || mask[x] == '?')
+                        {
+                            if (x == pattern.Length - 1)
+                                return startAddress + i;
+                            continue;
+                        }
+                        break;
+                    }
+                }
+                return -1;
+            }
+
+
+            [DllImport("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+            private static extern bool WriteProcessMemory2(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, [Out] int lpNumberOfBytesWritten);
+
+            [DllImport("kernel32")]
+            public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out IntPtr lpThreadId);
+            
+            [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+            static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint dwFreeType);
+
+
+
+        #region |- READ MEMORY -|
+
+        // Read Pointer
+        public Int32 ReadPointerInt(Int64 add, Int64[] offsets, int level)
+        {
+            Int64 lvl = add;
+            for (int i = 0; i < level; i++)
+            {
+                lvl = ReadInt64(lvl) + offsets[i];
+            }
+            //Console.WriteLine(lvl.ToString("X"));
+            return ReadInt32(lvl);
+        }
+
+        public Int64 GetPointerInt(Int64 add, Int64[] offsets, int level)
+        {
+            Int64 lvl = add;
+            for (int i = 0; i < level; i++)
+            {
+                lvl = ReadInt64(lvl) + offsets[i];
+            }
+            //Console.WriteLine(lvl.ToString("X"));
+            return lvl;
+        }
+
+        // Read Int32
+        public Int32 ReadInt32(Int64 _lpBaseAddress)
+            {
+                byte[] Buffer = new byte[4];
+                IntPtr ByteRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, 4, out ByteRead);
+
+                return BitConverter.ToInt32(Buffer, 0);
+            }
+
+            // Read UInt32
+            public UInt32 ReadUInt32(Int64 _lpBaseAddress)
+            {
+                byte[] Buffer = new byte[4];
+                IntPtr ByteRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, 4, out ByteRead);
+
+                return BitConverter.ToUInt32(Buffer, 0);
+            }
+
+            // Read Int64
+            public Int64 ReadInt64(Int64 _lpBaseAddress)
+            {
+                byte[] Buffer = new byte[8];
+                IntPtr ByteRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, 8, out ByteRead);
+
+                return BitConverter.ToInt64(Buffer, 0);
+            }
+
+            // Read UInt64
+            public UInt64 ReadUInt64(Int64 _lpBaseAddress)
+            {
+                byte[] Buffer = new byte[8];
+                IntPtr ByteRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, 8, out ByteRead);
+
+                return BitConverter.ToUInt64(Buffer, 0);
+            }
+
+            // Read Int64
+            public float ReadFloat(Int64 _lpBaseAddress)
+            {
+                byte[] Buffer = new byte[sizeof(float)];
+                IntPtr ByteRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, sizeof(float), out ByteRead);
+
+                return BitConverter.ToSingle(Buffer, 0);
+            }
+
+            // Read String
+            public string ReadString(Int64 _lpBaseAddress, UInt64 _Size)
+            {
+                byte[] Buffer = new byte[_Size];
+                IntPtr BytesRead;
+
+                memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, _Size, out BytesRead);
+
+                return Encoding.UTF8.GetString(Buffer);
+            }
+
+        public byte[] ReadXBytes(Int64 _lpBaseAddress, UInt64 nbytes)
+        {
+            byte[] Buffer = new byte[nbytes];
+            IntPtr ByteRead;
+
+            memory.ReadProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, nbytes, out ByteRead);
+
+            return Buffer;
+        }
+
+        #endregion
+
+
+        #region |- WRITE MEMORY -|
+
+        public void WriteMemory(Int64 MemoryAddress, byte[] Buffer)
+        {
+            uint oldProtect;
+            //IntPtr add;
+            memory.VirtualProtectEx(ProcessHandle, (IntPtr)MemoryAddress, (uint)Buffer.Length, (uint)memory.MemoryProtection.ExecuteReadWrite, out oldProtect);
+            //add = memory.VirtualAllocEx(ProcessHandle, (IntPtr)GetBaseAddress("s2_mp64_ship")+ 0x46F107, 0x800, (uint)memory.AllocationType.Reserve | (uint)memory.AllocationType.Commit, (uint)memory.MemoryProtection.ExecuteReadWrite);
+            //Console.WriteLine("allocation: {0:x}", add.ToInt64());
+            //Console.WriteLine(Marshal.GetLastWin32Error()); 
+
+            IntPtr ptrBytesWritten;
+            memory.WriteProcessMemory(ProcessHandle, MemoryAddress, Buffer, (uint)Buffer.Length, out ptrBytesWritten);
+            //memory.VirtualProtectEx(ProcessHandle, (IntPtr)MemoryAddress, (uint)Buffer.Length, oldProtect, out oldProtect);
+            //memory.VirtualFreeEx(ProcessHandle, add, UIntPtr.Zero, (uint)memory.AllocationType.Release);
+        }
+
+            public void WriteInt32(Int64 _lpBaseAddress, int _Value)
+            {
+                byte[] Buffer = BitConverter.GetBytes(_Value);
+
+                WriteMemory(_lpBaseAddress, Buffer);
+            }
+
+        public void WriteInt64(Int64 _lpBaseAddress, Int64 _Value)
+        {
+            byte[] Buffer = BitConverter.GetBytes(_Value);
+
+            WriteMemory(_lpBaseAddress, Buffer);
+        }
+
+        public void WriteUInt32(Int64 _lpBaseAddress, uint _Value)
+            {
+                byte[] Buffer = BitConverter.GetBytes(_Value);
+
+                WriteMemory(_lpBaseAddress, Buffer);
+            }
+
+            public void WriteFloat(Int64 _lpBaseAddress, float _Value)
+            {
+                byte[] Buffer = BitConverter.GetBytes(_Value);
+
+                WriteMemory(_lpBaseAddress, Buffer);
+            }
+
+            public void WriteByte(Int64 _lpBaseAddress, byte _Value)
+            {
+                byte[] Buffer = BitConverter.GetBytes(_Value);
+
+                IntPtr Zero = IntPtr.Zero;
+                WriteProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, (UInt32)Buffer.Length-1, out Zero);
+            }
+
+            public void WriteXBytes(Int64 _lpBaseAddress, byte[] _Value)
+            {
+                byte[] Buffer = _Value;
+            WriteMemory(_lpBaseAddress, Buffer);
+                IntPtr Zero = IntPtr.Zero;
+                //WriteProcessMemory(ProcessHandle, _lpBaseAddress, Buffer, (UInt32)Buffer.Length, out Zero);
+            }
+
+            public void WriteString(Int64 Address, string Text)
+            {
+                byte[] Buffer = new ASCIIEncoding().GetBytes(Text);
+                IntPtr Zero = IntPtr.Zero;
+                WriteProcessMemory(ProcessHandle, Address, Buffer, (uint)ReadString(Address,1024).Length , out Zero);
+            }
+
+            public void WriteNOP(Int64 Address)
+            {
+                byte[] Buffer = new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90 };
+                IntPtr Zero = IntPtr.Zero;
+                WriteProcessMemory(ProcessHandle, Address, Buffer, (UInt32)Buffer.Length, out Zero);
+            }
+            #endregion
+        }
+}
